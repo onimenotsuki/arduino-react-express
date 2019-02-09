39dccdd0a0646bd91c1958b506ccb4e0eb9c6678
Edgar Talledos <onimenotsuki@gmail.com>
2019-02-08 18:04:36 -0600

    Add tooglePower function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { isPower: false };
+  }
+
+  togglePower() {
+    const action = !this.state.isPower ? 'on' : 'off';
+
+    fetch(`http://localhost:8080/led?power=${action}`)
+      .then(_res => {
+        console.log(_res);
+
+        this.setState(prevState => ({
+          isPower: !prevState.isPower,
+        }));
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Aquí editaremos el código
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
